41c63b34da10bd547ec86cba227ec60633ea10db
eddy <eddy.mwaniki.27@gmail.com>
2025-03-10 01:13:55 +0300

    completes code challenge

--- lib/models.py
+++ lib/models.py
@@ -1,10 +1,62 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
+from sqlalchemy import ForeignKey, Column, Integer, String, create_engine, Boolean
 from sqlalchemy.orm import relationship, backref
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 
-convention = {
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-}
-metadata = MetaData(naming_convention=convention)
+Base = declarative_base()
+engine = create_engine("sqlite:///auditions_role.db")
 
-Base = declarative_base(metadata=metadata)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+class Audition(Base):
+    __tablename__ = "auditions"
+
+    id = Column(Integer(), primary_key = True)
+    actor = Column(String())
+    location = Column(String())
+    phone = Column(Integer())
+    hired = Column(Boolean(), default = False)
+
+    role_id = Column(Integer(), ForeignKey("roles.id"))
+    role = relationship("Role", back_populates = "auditions")
+
+    def call_back(self) :
+        self.hired = True
+        session.add(self)
+        session.commit()
+    def __repr__(self):
+        return f"ACTOR : {self.id} : {self.actor} PHONE : {self.phone}"
+
+class Role(Base):
+    __tablename__ = "roles"
+
+    id = Column(Integer(), primary_key = True)
+    character_name = Column(String())
+
+    auditions = relationship("Audition", back_populates = "role")
+
+    @property
+    def actors(self):
+        return [audition.actor for audition in self.auditions]  
+
+    @property
+    def locations(self):
+        return [audition.location for audition in self.auditions]
+
+    def lead(self) :
+        hired_auditions = [audition for audition in self.auditions if audition.hired == True]  
+        return hired_auditions[0] if hired_auditions else "no actor has been hired for this role."
+
+    def understudy(self):
+        hired_auditions = [audition for audition in self.auditions if audition.hired == True]
+        return hired_auditions[1] if len(hired_auditions) > 1 else "no actor has been hired for understudy for this role."          
+
+    def __repr__(self) :
+        return f"CHARACTER : {self.character_name}"       
+
+Base.metadata.create_all(engine)        
+
+
+
+
+
